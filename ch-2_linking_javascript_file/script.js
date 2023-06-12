c7b859a6fe4ec0e47afaa409ea4fa19533d43e75
kumarlalitearns6767 <45649399+lalitkumarthakur@users.noreply.github.com>
2023-06-12 19:21:19 +0530

    test commit

--- ch-2_linking_javascript_file/script.js
+++ ch-2_linking_javascript_file/script.js
@@ -1,10 +1,3 @@
 'use strict';
 
-let js = 'amazing'; // variable declaration.
-if (js === 'amazing')
-    console.log(' Javascript is Amazing ');
-else
-    document.write('Javascript is tricky');
-// if else condition.
-
-console.log(30 + 20 + 11115 + 858546923);
+console.log('No javascript files are available');
---
+++ ch-3_valuesAnd_variables/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let fname = "Lalit";
+const age = "22";
+var havas = "Too high";
+let relationship = "single";
+
+console.log(fname + " is " + age + " years old whose hawas " + havas + " and relationship statis is " + relationship);
