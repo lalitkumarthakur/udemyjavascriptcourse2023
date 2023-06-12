1e3bf75a9078ee1b0938db7f1ea42f4c88b662d4
kumarlalitearns6767 <45649399+lalitkumarthakur@users.noreply.github.com>
2023-06-12 13:47:41 +0530

    added 1st chapter

---
+++ ch-2_linking_javascript_file/script.js
@@ -0,0 +1,10 @@
+alert(' Hello World!');
+
+let js = 'amazing'; // variable declaration.
+if (js === 'amazing')
+    console.log(' Javascript is Amazing ');
+else
+    document.write('Javascript is tricky');
+// if else condition.
+
+console.log(30 + 20 + 11115 + 858546923);
